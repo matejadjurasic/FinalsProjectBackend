b064856d8a86ffeb9aece59f89d9ca917427b294
matejadjurasic <152192684+matejadjurasic@users.noreply.github.com>
2025-05-01 19:41:28 +0200

    added mediator to auth controller

---
+++ FitnessPal.Application/Features/Auth/Handlers/Commands/LoginCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using FitnessPal.Application.Contracts.Infrastructure;
+using FitnessPal.Application.Features.Auth.Requests.Commands;
+using FitnessPal.Application.Models.Identity;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessPal.Application.Features.Auth.Handlers.Commands
+{
+    public class LoginCommandHandler : IRequestHandler<LoginCommand, AuthResponse>
+    {
+        private readonly IAuthService _authService;
+
+        public LoginCommandHandler(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task<AuthResponse> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            var authResponse = await _authService.Login(request.authRequest!);
+            return authResponse;
+        }
+    }
+}
